67d142a20b37b191daeec6cd0c8a9eac8d42c423
Robert Horn <rhorn@mitre.org>
2018-08-27 15:06:00 -0400

    add sample NLP results for debugging

--- app/src/main/java/org/mitre/fluxnotes/MainActivity.kt
+++ app/src/main/java/org/mitre/fluxnotes/MainActivity.kt
@@ -10,6 +10,80 @@ import android.support.v4.widget.DrawerLayout
 import android.view.MenuItem
 import android.webkit.WebView
 
+const val SAMPLE_RESULTS = "{\n" +
+        "    \"diseaseStatus\": [\n" +
+        "        [\n" +
+        "            \"stable\"\n" +
+        "        ]\n" +
+        "    ],\n" +
+        "    \"toxicity\": [\n" +
+        "        {\n" +
+        "            \"analyzed_text\": \" of the the new regiment that we started six weeks ago. Right right. Gotcha. Um,Certainly notice. These These toxicities are side effects the muscle that you're describing. Um, the you mentioned tingling. Yeah, so the peripheral sensory neuropathy. Those are common side effects are toxicities that are associated with those two medications. Um, it's something that certainly be concerned. Mm. I guess a \",\n" +
+        "            \"concepts\": [\n" +
+        "                {\n" +
+        "                    \"dbpedia_resource\": \"http://dbpedia.org/resource/Myalgia\",\n" +
+        "                    \"relevance\": 0.847421,\n" +
+        "                    \"text\": \"Myalgia\"\n" +
+        "                },\n" +
+        "                {\n" +
+        "                    \"dbpedia_resource\": \"http://dbpedia.org/resource/Peripheral_neuropathy\",\n" +
+        "                    \"relevance\": 0.82593,\n" +
+        "                    \"text\": \"Peripheral neuropathy\"\n" +
+        "                }\n" +
+        "            ],\n" +
+        "            \"entities\": [],\n" +
+        "            \"language\": \"en\",\n" +
+        "            \"usage\": {\n" +
+        "                \"features\": 2,\n" +
+        "                \"text_characters\": 404,\n" +
+        "                \"text_units\": 1\n" +
+        "            }\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"analyzed_text\": \"ng that's that we think is going to happen for a couple of weeks or it's a great question. So hopefully these side effects will not get worse and to hopefully subside because they're related. to the medication, soumIs aren't um likel\",\n" +
+        "            \"concepts\": [],\n" +
+        "            \"entities\": [],\n" +
+        "            \"language\": \"en\",\n" +
+        "            \"usage\": {\n" +
+        "                \"features\": 2,\n" +
+        "                \"text_characters\": 233,\n" +
+        "                \"text_units\": 1\n" +
+        "            }\n" +
+        "        },\n" +
+        "        {\n" +
+        "            \"analyzed_text\": \" Um, it's not progressing. It is stable. That's a very good thing. So my recommendation is let's document the toxicities. Let's make sure we understand that the and peripheral neuropathy I things to be concerned about in to watch a\",\n" +
+        "            \"concepts\": [\n" +
+        "                {\n" +
+        "                    \"dbpedia_resource\": \"http://dbpedia.org/resource/Peripheral_neuropathy\",\n" +
+        "                    \"relevance\": 0.949819,\n" +
+        "                    \"text\": \"Peripheral neuropathy\"\n" +
+        "                }\n" +
+        "            ],\n" +
+        "            \"entities\": [\n" +
+        "                {\n" +
+        "                    \"count\": 1,\n" +
+        "                    \"disambiguation\": {\n" +
+        "                        \"dbpedia_resource\": \"http://dbpedia.org/resource/Peripheral_neuropathy\",\n" +
+        "                        \"name\": \"Peripheral neuropathy\",\n" +
+        "                        \"subtype\": [\n" +
+        "                            \"Disease\"\n" +
+        "                        ]\n" +
+        "                    },\n" +
+        "                    \"relevance\": 0.986086,\n" +
+        "                    \"text\": \"peripheral neuropathy\",\n" +
+        "                    \"type\": \"HealthCondition\"\n" +
+        "                }\n" +
+        "            ],\n" +
+        "            \"language\": \"en\",\n" +
+        "            \"usage\": {\n" +
+        "                \"features\": 2,\n" +
+        "                \"text_characters\": 231,\n" +
+        "                \"text_units\": 1\n" +
+        "            }\n" +
+        "        }\n" +
+        "    ]\n" +
+        "}\n"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mDrawerLayout: DrawerLayout
@@ -60,6 +134,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.nav_display_sample -> {
                     val intent = Intent(this, DisplayResultsActivity::class.java)
+                    intent.putExtra("text", SAMPLE_RESULTS)
                     startActivity(intent)
                 }
             }
